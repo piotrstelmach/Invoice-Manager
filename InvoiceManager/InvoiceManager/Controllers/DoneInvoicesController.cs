089c8a3361347c123697447a26bad9c1ed6fd186
Piotr <stelmachpiotr46@gmail.com>
2019-03-21 22:26:04 +0100

    Customizing main view and add controllers to needed functionality

---
+++ InvoiceManager/InvoiceManager/Controllers/DoneInvoicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoiceManager.Controllers
+{
+    public class DoneInvoicesController : Controller
+    {
+        // GET: DoneInvoices
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
